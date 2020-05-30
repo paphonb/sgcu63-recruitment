bbdf9905a3235ae436dc34af7960753afbabdfd0
paphonb <paphonb@gmail.com>
2020-05-30 17:10:58 +0700

    P3: implement magnification

--- Prob#3 - Numericalization/numericalization.js
+++ Prob#3 - Numericalization/numericalization.js
@@ -1,13 +1,31 @@
 const font = require('./font')
 
+// function to write a char for n times to magnify horizontally
+function write(char, times) {
+  while (times--) {
+    process.stdout.write(char)
+  }
+}
+
 module.exports = function numericalization(number, vertCount, horzCount) {
   for (let y = 0; y < 5; y++) {
-    for (let num of number) {
-      for (let x = 0; x < 5; x++) {
-        process.stdout.write(font[num][y][x])
+    // magnify vertically
+    for (let subY = 0; subY < vertCount; subY++) {
+      let isFirst = true
+      for (let num of number) {
+        // print the spacer
+        if (isFirst) {
+          isFirst = false
+        } else {
+          write(' ', horzCount)
+        }
+        
+        // then print the numbers
+        for (let x = 0; x < 5; x++) {
+          write(font[num][y][x], horzCount)
+        }
       }
-      process.stdout.write(' ')
+      process.stdout.write('\n')
     }
-    process.stdout.write('\n')
   }
 }

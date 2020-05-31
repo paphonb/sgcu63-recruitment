e63751964707eca80504c074ccc8c3efe1fdedcb
paphonb <paphonb@gmail.com>
2020-05-31 21:16:35 +0700

    P3: implement line wrapping

--- Prob#3 - Numericalization/numericalization.js
+++ Prob#3 - Numericalization/numericalization.js
@@ -7,7 +7,7 @@ function write(char, times) {
   }
 }
 
-module.exports = function numericalization(number, vertCount, horzCount) {
+function printLine(number, vertCount, horzCount) {
   for (let y = 0; y < 5; y++) {
     // magnify vertically
     for (let subY = 0; subY < vertCount; subY++) {
@@ -27,5 +27,26 @@ module.exports = function numericalization(number, vertCount, horzCount) {
       }
       process.stdout.write('\n')
     }
+  }
+}
+
+module.exports = function numericalization(number, vertCount, horzCount) {
+  // additional idea: if the number doesn't fit in one line, display in 
+  //                  in multiple lines separated by M1 newlines
+  const width = process.stdout.columns || MAX_SAFE_INTEGER
+  let maxLength = Math.floor((width + 1) / (horzCount * 6))
+  if (maxLength === 0) {
+    // it will overflow with 1 number anyway
+    maxLength = MAX_SAFE_INTEGER
+  }
+  const lines = number.match(new RegExp(`.{1,${maxLength}}`, 'g'))
+  let isFirst = true
+  for (const line of lines) {
+    if (isFirst) {
+      isFirst = false
+    } else {
+      write('\n', vertCount)
+    }
+    printLine(line, vertCount, horzCount)
   }
 }
